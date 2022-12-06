21dbdc5959e802daf693981ef4505ff65de667ff
MinhKun912 <109707497+MinhKun912@users.noreply.github.com>
2022-12-06 20:50:53 +0700

    Add files via upload

---
+++ vcl.js
@@ -0,0 +1,90 @@
+function nutBam(){
+    let inp=+document.getElementById('nhap1').value;
+    switch (inp){
+        case 1:
+          document.getElementById('dis1').innerHTML="Có 31 ngày";
+          break;
+        case 2:
+            document.getElementById('dis1').innerHTML="Có 29 hoặc 28 ngày";
+            break;
+        case 3:
+            document.getElementById('dis1').innerHTML="Có 30 ngày";
+            break;
+        case 4:
+            document.getElementById('dis1').innerHTML="Có 31 ngày";
+            break;
+        case 5:
+            document.getElementById('dis1').innerHTML="Có 30 ngày";
+            break;
+        case 6:
+            document.getElementById('dis1').innerHTML="Có 31 ngày";
+            break;
+        case 7:
+            document.getElementById('dis1').innerHTML="Có 31 ngày";
+            break;
+        case 8:
+            document.getElementById('dis1').innerHTML="Có 31 ngày";
+            break;
+        case 9:
+            document.getElementById('dis1').innerHTML="Có 30 ngày";
+            break;
+        case 10:
+            document.getElementById('dis1').innerHTML="Có 31 hoặc ngày";
+            break;
+        case 11:
+            document.getElementById('dis1').innerHTML="Có 30 ngày";
+            break;
+        case 12:
+            document.getElementById('dis1').innerHTML="Có 31 ngày";
+            break;
+
+        default:
+            document.getElementById('dis1').innerHTML="1 năm có 12 tháng thôi! Đớ !"
+    }
+}
+
+
+function nutBam2(){
+    let inp2=+document.getElementById('nhap2').value;
+    switch (inp2){
+        case 1:
+            document.getElementById('dis2').innerHTML="Có 31 ngày";
+            break;
+        case 2:
+            document.getElementById('dis2').innerHTML="Có 29 hoặc 28 ngày";
+            break;
+        case 3:
+            document.getElementById('dis2').innerHTML="Có 31 ngày";
+            break;
+        case 4:
+            document.getElementById('dis2').innerHTML="Có 30 ngày";
+            break;
+        case 5:
+            document.getElementById('dis2').innerHTML="Có 31 ngày";
+            break;
+        case 6:
+            document.getElementById('dis2').innerHTML="Có 30 ngày";
+            break;
+        case 7:
+            document.getElementById('dis2').innerHTML="Có 31 ngày";
+            break;
+        case 8:
+            document.getElementById('dis2').innerHTML="Có 31 ngày";
+            break;
+        case 9:
+            document.getElementById('dis2').innerHTML="Có 30 ngày";
+            break;
+        case 10:
+            document.getElementById('dis2').innerHTML="Có 31 ngày";
+            break;
+        case 11:
+            document.getElementById('dis2').innerHTML="Có 30 ngày";
+            break;
+        case 12:
+            document.getElementById('dis2').innerHTML="Có 31 ngày";
+            break;
+
+        default:
+            document.getElementById('dis2').innerHTML="1 năm có 12 tháng thôi! Đớ !"
+    }
+}
